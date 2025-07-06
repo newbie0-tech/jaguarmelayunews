6f199c9109d9d0e61cf47832f2652fa015039921
newbie0-tech <indonesialinkid@gmail.com>
2025-07-07 05:53:00 +0700

    Update db.php

--- portal/inc/db.php
+++ portal/inc/db.php
@@ -1,10 +1,7 @@
-<?php
-$host = 'localhost';
-$user = 'berita_user';    
-$pass = 'Berita$2025';     
-$db   = 'portal_db';
-
-$conn = new mysqli($host, $user, $pass, $db);
-if ($conn->connect_error) die('DB gagal: '.$conn->connect_error);
-mysqli_set_charset($conn,'utf8mb4');
-?>
+$mysqli = new mysqli(
+    getenv('DB_HOST'),
+    getenv('DB_USER'),
+    getenv('DB_PASS'),
+    getenv('DB_NAME'),
+    getenv('DB_PORT') ?: 3306
+);
