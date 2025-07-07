6cdad6bce1782e92696f70cb38d2c8fb659e55bc
mia_ardilasyah <indonesialinkid@gmail.com>
2025-07-07 07:30:23 +0700

    db.php

--- portal/inc/db.php
+++ portal/inc/db.php
@@ -4,11 +4,11 @@ ini_set('display_errors',1);
 error_reporting(E_ALL);
 
 $conn = new mysqli(
-    getenv('DB_HOST'),
-    getenv('DB_USER'),
-    getenv('DB_PASS'),
-    getenv('DB_NAME'),
-    getenv('DB_PORT') ?: 3306
+    getenv('containers-us-west-1.railway.internal'),
+    getenv('railway'),
+    getenv('linkid99'),
+    getenv('railway'),
+    getenv('3306') ?: 3306
 );
 
 if ($conn->connect_error) {
