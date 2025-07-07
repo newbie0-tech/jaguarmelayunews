01265162b8c2abe5a89ef2b69e8fa1fb7eb4c62b
mia_ardilasyah <indonesialinkid@gmail.com>
2025-07-07 07:24:13 +0700

    db.php

--- portal/inc/db.php
+++ portal/inc/db.php
@@ -1,10 +1,8 @@
 <?php
-// Aktifkan laporan error saat pengembangan (nonaktifkan di produksi)
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
+// db.php
+ini_set('display_errors',1);
 error_reporting(E_ALL);
 
-// Buat koneksi ke database
 $conn = new mysqli(
     getenv('DB_HOST'),
     getenv('DB_USER'),
@@ -13,7 +11,6 @@ $conn = new mysqli(
     getenv('DB_PORT') ?: 3306
 );
 
-// Cek koneksi
 if ($conn->connect_error) {
-    die('Koneksi database gagal: ' . $conn->connect_error);
+    die('Koneksi gagal: '.$conn->connect_error);
 }
