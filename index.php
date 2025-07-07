4c3a288cc1181c9b4187a224d6b69611bcfe7982
mia_ardilasyah <indonesialinkid@gmail.com>
2025-07-07 07:15:26 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,4 @@
 
 <?php
-// Redirect permanen (HTTP 302 → 307 tergantung browser)
-// Ganti '/berita/login.php' jika jalurnya berbeda.
 header('Location: /portal/index.php', true, 302);
-exit;   // pastikan tak ada output lain setelah ini
+exit;
