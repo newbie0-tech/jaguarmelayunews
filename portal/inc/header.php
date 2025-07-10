e963bedd22de01482a839560c51d52f3eb72172d
mia_ardilasyah <indonesialinkid@gmail.com>
2025-07-10 18:15:08 +0700

    Update header.php

--- portal/inc/header.php
+++ portal/inc/header.php
@@ -1,6 +1,6 @@
 <link rel="stylesheet" href="/portal/css/header.css">
 <header class="site-header">
-  <div class="header-top">
+  <div class="top-bar">
     <div class="logo-title">
       <img src="/portal/assets/logo.png" alt="Logo" class="logo">
       <h1 class="site-title">
@@ -8,20 +8,31 @@
         <span class="stroke-green">Melayu News</span>
       </h1>
     </div>
-    <div class="ads-row">
-      <img src="/portal/assets/ads/iklan1.png" alt="Iklan 1" class="ad-banner">
-      <img src="/portal/assets/ads/iklan2.png" alt="Iklan 2" class="ad-banner">
-      <img src="/portal/assets/ads/iklan3.png" alt="Iklan 3" class="ad-banner">
+
+    <div class="search-social">
+      <form action="/portal/search.php" method="get" class="search-form">
+        <input type="text" name="q" placeholder="Cari berita...">
+        <button type="submit">🔍</button>
+      </form>
+      <div class="social-icons">
+        <a href="#"><img src="/portal/assets/icons/fb.png" alt="Facebook"></a>
+        <a href="#"><img src="/portal/assets/icons/x.png" alt="Twitter/X"></a>
+        <a href="#"><img src="/portal/assets/icons/yt.png" alt="YouTube"></a>
+      </div>
     </div>
   </div>
+
+  <div class="breaking-marquee">
+    <span class="breaking-flash">🚨 Breaking News</span>
+    <marquee scrollamount="6">Polda Riau gelar Bhayangkara Run 2025 • Gunung Lewotobi Meletus 7 Juli 2025 • LAMR Riau Jangan Terima Bersih</marquee>
+  </div>
+
   <nav class="main-nav">
-    <ul>
-      <li><a href="/portal/">Beranda</a></li>
-      <li><a href="/portal/kategori.php?kat=nasional">Nasional</a></li>
-      <li><a href="/portal/kategori.php?kat=daerah">Daerah</a></li>
-      <li><a href="/portal/kategori.php?kat=politik">Politik</a></li>
-      <li><a href="/portal/kategori.php?kat=pendidikan">Pendidikan</a></li>
-      <li><a href="/portal/kategori.php?kat=dunia">Dunia</a></li>
-    </ul>
+    <a href="/portal/">Beranda</a>
+    <a href="#">Nasional</a>
+    <a href="#">Daerah</a>
+    <a href="#">Politik</a>
+    <a href="#">Pendidikan</a>
+    <a href="#">Dunia</a>
   </nav>
 </header>
