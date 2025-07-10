d9c2efef8e9dd0d698a68dd64651a888d6d47b93
mia_ardilasyah <indonesialinkid@gmail.com>
2025-07-10 16:22:53 +0700

    Update index.php

--- portal/index.php
+++ portal/index.php
@@ -69,7 +69,7 @@ $populer = $conn->query("SELECT judul,slug FROM posts WHERE status=1 ORDER BY vi
   <aside class="sidebar">
     <h3>Jaguar Channel</h3>
     <div style="position:relative;padding-bottom:56.25%;height:0;overflow:hidden;">
-      <iframe src="https://www.youtube.com/embed/@teamgampemburusejarahmelay3314" frameborder="0" allowfullscreen style="position:absolute;top:0;left:0;width:100%;height:100%;"></iframe>
+      <iframe src="https://www.youtube.com/@teamgampemburusejarahmelay3314" frameborder="0" allowfullscreen style="position:absolute;top:0;left:0;width:100%;height:100%;"></iframe>
     </div>
     <div class="share-btns" style="margin-top:10px;">
       <li><a class="wa" href="https://wa.me/?text=Simak%20video%20ini%20https://youtu.be/" target="_blank">WhatsApp</a></div></li>
@@ -78,19 +78,7 @@ $populer = $conn->query("SELECT judul,slug FROM posts WHERE status=1 ORDER BY vi
     </div>
   </aside>
 <div class="main-grid">
-  <!-- Sidebar KIRI: YouTube -->
-  <aside class="sidebar">
-    <h3>Vitron<h3>
-    <div style="position:relative;padding-bottom:56.25%;height:0;overflow:hidden;">
-      <iframe src="https://www.youtube.com/embed/@teamgampemburusejarahmelay3314" frameborder="0" allowfullscreen style="position:absolute;top:0;left:0;width:100%;height:100%;"></iframe>
-    </div>
-    <div class="share-btns" style="margin-top:10px;">
-      <li><a class="wa" href="https://wa.me/?text=Simak%20video%20ini%20https://youtu.be/" target="_blank">WhatsApp</a></div></li>
-      <li><a class="fb" href="https://www.facebook.com/sharer/sharer.php?u=https://youtu.be/VIDEO_ID_KAMU" target="_blank">Facebook</a></li>
-      <li><a class="tt" href="https://www.tiktok.com/@jaguarmelayunews" target="_blank">TikTok</a></li>
-    </div>
-  </aside>
-  <!-- Konten utama berita -->
+    <!-- Konten utama berita -->
   <div class="news-content">
     <?php foreach($categories as $cat):
       $stmt = $conn->prepare("
